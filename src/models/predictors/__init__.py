c3cad977c350ca240c9fe1ad7ae586bdc190412c
Shahar Harel <shahar@quris.ai>
2025-11-25 01:35:08 +0200

    stealth

--- src/models/predictors/__init__.py
+++ src/models/predictors/__init__.py
@@ -2,7 +2,7 @@
 
 from .property_predictor import PropertyPredictor, PropertyPredictorMLP
 from .edit_effect_predictor import EditEffectPredictor, EditEffectMLP
-from .structured_edit_effect_predictor import StructuredEditEffectPredictor, StructuredEditEffectMLP
+from .structured_edit_effect_predictor_v2 import StructuredEditEffectPredictor, StructuredEditEffectMLP
 
 __all__ = [
     'PropertyPredictor',
